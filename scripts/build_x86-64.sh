c53f006c4802515ac9c2fd7b3b379d546d7007c8
Matheus Pi <matheusgarcia28@gmail.com>
2021-12-02 18:36:55 +0000

    Adding build stage.

---
+++ scripts/build_x86-64.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+: ' This is the build helper script. This script will invoke
+    the Conan installation, followed by CMake configuration
+    then the build command.
+'
+
+THIS_DIR=$(cd "$(dirname "$0")" && pwd)
+PROJECT_DIR="$(cd "${THIS_DIR}/.." && pwd)"
+BUILD_DIR="${PROJECT_DIR}/build"
+
+
+# Install all dependencies
+${THIS_DIR}/conan-install.sh
+
+# configure the project
+cmake \
+  -B "${BUILD_DIR}" \
+  -DCMAKE_MODULE_PATH:FILEPATH="${BUILD_DIR}" \
+  ${PROJECT_DIR}
+
+
+cmake \
+  --build ${BUILD_DIR} \
+  -j"$(nproc)"
+  
---
+++ scripts/helpers/conan-install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+: ' This file will install all the conan dependencies for the
+    x86_64 architecture. In the future, this should use the
+    Conan profiles.
+'
+BUILD_DIR="$(cd "$(dirname "$0")/.." && pwd)/build"
+
+# Get the profile and save to file
+PROFILE_FILE="${BUILD_DIR}/ubuntu_x86-64_gcc10"
+
+if [ -f ${PROFILE_FILE} ]; then
+  rm ${PROFILE_FILE}
+fi
+conan profile new "${PROFILE_FILE}"  --detect
+conan profile update settings.compiler.libcxx=libstdc++11 "${PROFILE_FILE}"
+conan profile update settings.build_type=Debug "${PROFILE_FILE}"
+
+# Install all dependencies
+conan install \
+  --install-folder="${BUILD_DIR}" \
+  --build=missing \
+  --profile="${PROFILE_FILE}" \
+  ..
