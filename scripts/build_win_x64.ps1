69a00ae8aec424e0a4c8145e2235be56b858e5d7
Matheus Gomes <matheusgarcia28@gmail.com>
2023-03-18 10:05:45 +0000

    calling cmake preset build without the dir.

--- scripts/build_win_x64.ps1
+++ scripts/build_win_x64.ps1
@@ -3,5 +3,5 @@ $ProjectDir = Split-Path -Parent $PSScriptRoot
 cmake "$ProjectDir" --preset="vs2022-deb"
 if(!$?) { Exit $LASTEXITCODE }
 
-cmake --build "$ProjectDir" --preset="vs2022-deb"
+cmake --build --preset="vs2022-deb"
 if(!$?) { Exit $LASTEXITCODE }
