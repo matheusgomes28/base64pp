bfcb641168cb9b1fca6171ce2590130e098fbeac
Matheus Gomes <matheusgarcia28@gmail.com>
2023-03-18 00:04:34 +0000

    testing output.

--- scripts/helpers/conan-install.ps1
+++ scripts/helpers/conan-install.ps1
@@ -1,20 +1,48 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Debug","Release")]$BuildType
+)   
+
 # Need "if(!$?) { Exit $LASTEXITCODE }" after
 # every external command call as everything
 # needs to work for the build
 
 # Install conan with chocolatey
-choco install conan --version=2.0.1 /PathType="Machine"
-if(!$?) { Exit $LASTEXITCODE }
-RefreshEnv
+choco install conan --version=2.0.1 --installargs '/PathType:Machine' -y --no-progress
 if(!$?) { Exit $LASTEXITCODE }
 
-$ProjectDir = Split-Path -Parent $PSScriptRoot
+# Set this because refreshenv doesn't work
+# on the same powershell session
+if (-not(Get-Command conan -ErrorAction SilentlyContinue))
+{
+    New-Alias -Name conan -Value "$Env:ProgramFiles\Conan\conan\conan.exe"
+}
 
 conan profile detect --force
 if(!$?) { Exit $LASTEXITCODE }
 
+$ConanProfileFile = "$(conan profile path default)"
+if(!$?) { Exit $LASTEXITCODE }
+
+# Update the conan profiles to build for
+# debug with MTd runtime on MSVC
 $ConanProfile = Get-Content -Path "$(conan profile path default)"
 if(!$?) { Exit $LASTEXITCODE }
 $ConanProfile = $ConanProfile -Replace "build_type=Debug", "build_type=Release"
 $ConanProfile = $ConanProfile -Replace "compiler.runtime=dynamic", "compiler.runtime=static"
-Set-Content -Path $ConanProfile -Value $ConanProfile
+Write-Output "---- New conan profile -----"
+Set-Content -Path $ConanProfileFile -Value $ConanProfile
+Write-Output $ConanProfile
+
+# Install all dependencies
+$ProjectDir = Split-Path -Parent "$PSScriptRoot"
+$ConanOutputDir = "${ProjectDir}/conan/deb"
+if ($BuildType -eq "Release")
+{
+    $ConanOutputDir = "${ProjectDir}/conan/rel"
+}
+
+conan install \
+  --output-folder="${ConanOutputDir}" \
+  --build=missing \
+  "${ProjectDir}"
