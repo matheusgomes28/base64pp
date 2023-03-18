a8a2fe3b3be754650827fdc81dc80f7f401474d6
Matheus Gomes <matheusgarcia28@gmail.com>
2023-03-18 18:57:20 +0000

    Compile flags.

--- scripts/build_win_x64.ps1
+++ scripts/build_win_x64.ps1
@@ -1,6 +1,6 @@
 $ProjectDir = Split-Path -Parent $PSScriptRoot
 
-cmake "$ProjectDir" --preset="vs2022-deb"
+cmake "$ProjectDir" --preset="vs2022-deb" -DCMAKE_VERBOSE_MAKEFILE="True"
 if(!$?) { Exit $LASTEXITCODE }
 
 cmake --build --preset="vs2022-deb"
