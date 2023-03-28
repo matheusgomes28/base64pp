62a9112b6d53a79bf4cc7162584bee7796421d91
matheusgomes28 <matheusgarcia28@gmail.com>
2023-03-28 11:36:34 +0100

    Added windows release building and artifacts.

--- scripts/build_win_x64.ps1
+++ scripts/build_win_x64.ps1
@@ -1,7 +1,12 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("vs2022-deb","vs2022-rel")]$Preset
+)   
+
 $ProjectDir = Split-Path -Parent $PSScriptRoot
 
-cmake "$ProjectDir" --preset="vs2022-deb"
+cmake "$ProjectDir" --preset="$Preset"
 if(!$?) { Exit $LASTEXITCODE }
 
-cmake --build --preset="vs2022-deb"
+cmake --build --preset="$Preset"
 if(!$?) { Exit $LASTEXITCODE }
