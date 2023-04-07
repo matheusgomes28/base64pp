9522a93098a6e360c78cf5e9ab2e512b02d12e9e
matheusgomes28 <matheusgarcia28@gmail.com>
2023-04-07 12:40:04 +0100

    Fixing lint.

--- scripts/gather_cov.sh
+++ scripts/gather_cov.sh
@@ -20,10 +20,10 @@ CONAN_DIR="${HOME}/.conan2/"
 lcov --remove coverage.info "${SYSTEM_DIR}*" --output-file coverage.info
 lcov --remove coverage.info "${CONAN_DIR}*" --output-file coverage.info
 
-##
+# Generate HTML report into coverage/
 genhtml coverage.info --output-directory coverage > coverage_stats.txt
-LINES_COV="$(cat coverage_stats.txt | grep lines | grep -Eo "[0-9]+\.[0-9]+%" | cut -d '.' -f1)"
-FUNCTIONS_COV="$(cat coverage_stats.txt | grep functions | grep -Eo "[0-9]+\.[0-9]+%" | cut -d '.' -f1)"
+LINES_COV="$(grep lines < coverage_stats.txt | grep -Eo "[0-9]+\.[0-9]+%" | cut -d '.' -f1)"
+FUNCTIONS_COV="$(grep functions < coverage_stats.txt | grep -Eo "[0-9]+\.[0-9]+%" | cut -d '.' -f1)"
 
 # Make sure we fail the pipeline if coverage is less than something
 LINES_COV_THRESH="98"
@@ -34,6 +34,6 @@ fi
 
 FUNCTIONS_COV_THRESH="100"
 if [ "$FUNCTIONS_COV" -lt "$FUNCTIONS_COV_THRESH" ]; then
-    echo "Functions coverage is less than threshold $FUNCTION_COV_THRESH"
+    echo "Functions coverage is less than threshold $FUNCTIONS_COV_THRESH"
     exit 1
 fi
