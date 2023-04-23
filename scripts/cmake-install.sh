fde7c815bdc2952adf9ae8ab56f7c242bc75713c
matheusgomes28 <matheusgarcia28@gmail.com>
2023-04-23 19:45:10 +0100

    Add CMake Installing to Pipeline (#88)

---
+++ scripts/cmake-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+: ' This script preprares the environment for a
+    cmake installation. This assumes that we are
+    using one of the config/build presets given
+    by CMakePresets.json, given as an input in
+    this script.
+
+    Usage: ./cmake-install.sh PRESET_NAME
+'
+
+if [ "$#" -lt 1 ]; then
+    echo "Call the script with build preset"
+    exit 1
+fi
+
+THIS_DIR=$(cd "$(dirname "$0")" && pwd)
+PROJECT_DIR="$(cd "${THIS_DIR}/.." && pwd)"
+BUILD_DIR="${PROJECT_DIR}/build/${1}"
+
+cmake --install "${BUILD_DIR}"
