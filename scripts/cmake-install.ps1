e5e2f97735e35a9638181193bb403c6cb4965b1d
matheusgomes28 <matheusgarcia28@gmail.com>
2023-04-26 13:52:08 +0100

    Adding the Windows install step to build stage. (#89)

---
+++ scripts/cmake-install.ps1
@@ -0,0 +1,19 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("vs2022-deb","vs2022-rel")]$Preset
+)   
+
+$ProjectDir = Split-Path -Parent $PSScriptRoot
+$BuildDir = Join-Path $ProjectDir "build\$Preset"
+
+# Hacky way to figure whether it's a
+# debug/release build as presets don't
+# support it just yet
+$Configuration = "Debug"
+if ($Preset -like "*rel*")
+{
+    $Configuration = "Release"
+}
+
+cmake --install "$BuildDir" --config $Configuration
+if(!$?) { Exit $LASTEXITCODE }
