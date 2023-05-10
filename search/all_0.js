90a6c42011f3ec8388035df0bc5c2071963f1244
Matheus Gomes <matheusgarcia28@gmail.com>
2023-05-10 14:07:01 +0100

    Updating the gh-pages with the new docs.

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['base64pp_0',['base64pp',['../namespacebase64pp.html',1,'']]],
-  ['base64pp_20_2d_20a_20modern_20c_2b_2b_20implementation_20of_20base64_20encoding_1',['Base64pp - A Modern C++ Implementation Of Base64 Encoding',['../index.html',1,'']]],
+  ['base64pp_1',['Base64pp',['../index.html',1,'']]],
   ['base64pp_2eh_2',['base64pp.h',['../base64pp_8h.html',1,'']]]
 ];
